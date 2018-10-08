ec1bff51dd60b840722dbe375561e4464df57868
brunohenriquebh <brunohenriquebh@gmail.com>
2018-10-08 12:34:38 -0300

    Update src/PHPHtmlParser/Selector.php
    Update to PHP 7.2 (validation array)

--- src/PHPHtmlParser/Selector.php
+++ src/PHPHtmlParser/Selector.php
@@ -168,10 +168,10 @@ class Selector
     protected function seek(array $nodes, array $rule, array $options)
     {
         // XPath index
-        if (count($rule['tag']) > 0 &&
-            count($rule['key']) > 0 &&
+        if (strlen($rule['tag']) > 0 &&
+            strlen($rule['key']) > 0 &&
             is_numeric($rule['key'])
-        ) {
+        )  {
             $count = 0;
             /** @var AbstractNode $node */
             foreach ($nodes as $node) {
